573bb4005aaaca31c32e5f6cdfa293e1c1dc108c
Tim <tim.maastricht@gmail.com>
2026-02-02 18:52:55 +1000

    added error handling

--- src/main.js
+++ src/main.js
@@ -3,23 +3,30 @@ function getPokemon() {
   // make web request to API
   
   //  save the respone from the API 
-  let response = fetch("https://pokeapi.co/api/v2/pokemon/25").then((response) => {
+  let response = fetch("https://keapi.co/api/v2/pokemon/25").then((response) => {
     console.log(response)
     return response.json();
-    
+
   }).then((jsonData) => {
     // process the saved response from the API
     console.log(jsonData);
-
-  });
+    
+    // create HTML elements that use the response from the API
+    let pokemonContainer = document.getElementById("pokemonDataContainer");
+    let pokemonNameHeader = document.createElement("h2");
+    pokemonNameHeader.innerText = jsonData.name;
+    
+    // add those HTML elements to the page
+    pokemonContainer.appendChild(pokemonNameHeader);
+  }).catch((error) => {
+    console.log(error);
+    console.log("resource does not exist");
+    return;
+  })
   console.log(response)
   
-  // create HTML elements that use the response from the API
   
-  // add those HTML elements to the page
 
 }
 
-
 getPokemon()
-// https://pokeapi.co/api/v2/pokemon
