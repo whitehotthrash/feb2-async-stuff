9ca8cb666b1c66166cda5f0c88bf0c6d6cff4162
Tim <tim.maastricht@gmail.com>
2026-02-02 19:47:38 +1000

    feat: added button and event listener to change pokemon

--- src/main.js
+++ src/main.js
@@ -20,6 +20,7 @@
 //     })
 //     .catch((error) => {
 //       console.log(error);
+//if (!response) {}
 // message: "nope"
 // statusCode: httpStatusCodes.clientError.UNAUTHORIZED
 //       console.log("resource does not exist");
@@ -32,18 +33,40 @@
 async function getPokemon() {
   // make web request to API
   // save the response
-  let response = await fetch("https://pokeapi.co/api/v2/pokemon/25");
+  let randomNumber = await delayedRandomNumber()
+  let response = await fetch("https://pokeapi.co/api/v2/pokemon/" + randomNumber);
   let bodyData = await response.json();
   //process the saved response from the API
   console.log(bodyData);
 
   // create HTML elements that use the response from the API
   let pokemonContainer = document.getElementById("pokemonDataContainer");
-  let pokemonNameHeader = document.createElement("h2");
+  // let pokemonNameHeader = document.createElement("h2");
+  let pokemonNameHeader = document.getElementById("pokemonName")
   pokemonNameHeader.innerText = bodyData.name;
 
   // add those HTML elements to the page
   pokemonContainer.appendChild(pokemonNameHeader);
 }
 // await fetch is async blocking
-getPokemon();
+//getPokemon();
+
+
+async function delayedRandomNumber(){
+
+  // async blocking delay within the function
+  await new Promise(resolve => setTimeout(resolve, Math.random() * 500));
+  return Math.floor(Math.random() * 1025) + 1;
+
+  // set timeout is non-blocking async
+  // setTimeout(() => {
+  //   return (Math.floor(Math.random()) * 1025) + 1;
+  // }, Math.random() * 2000)
+}
+
+const button = document.getElementById("pokeButton");
+button.addEventListener("click", function () {
+  getPokemon()
+})
+// create an event listener for the click on the button
+// to get a random pokemon
